6d5e6a1297f9415b09cc693d8514acb897746bdb
Jaspirr <jesper.erlandsson@hotmail.com>
2022-11-16 10:24:45 +0100

    added ts

---
+++ src/components/InputForm.tsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react'
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState('')
+  
+  return (
+    <div>: React.FC</div>
+  )
+}
+
+export default App;
